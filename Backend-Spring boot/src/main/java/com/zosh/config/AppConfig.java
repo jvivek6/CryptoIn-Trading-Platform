2a499384dc91da0f53257b2405608779ed8290f5
Vivek Jaiswal <jvivek608@gmail.com>
2025-02-03 22:57:51 +0530

    change api

--- Backend-Spring boot/src/main/java/com/zosh/config/AppConfig.java
+++ Backend-Spring boot/src/main/java/com/zosh/config/AppConfig.java
@@ -90,7 +90,7 @@ public class AppConfig {
 	                    "http://localhost:5173",
 						"http://localhost:5174",
 	                    "http://localhost:4200",
-							"https://zosh-treading.vercel.app"
+							"https://cryptoin-trade.vercel.app"
 	                ));
 	                cfg.setAllowedMethods(Collections.singletonList("*"));
 	                cfg.setAllowCredentials(true);
