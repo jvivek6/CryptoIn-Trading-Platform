bd79efc581b986e8885a003d2a585b42f33275bd
Vivek Jaiswal <jvivek608@gmail.com>
2025-02-06 22:23:13 +0530

    Your commit message

--- Frontend-React/src/Api/api.js
+++ Frontend-React/src/Api/api.js
@@ -2,16 +2,19 @@
 import axios from 'axios';
 
 
-export const API_BASE_URL = "cryptoin-server-production.up.railway.app"
+
+const API_BASE_URL = "cryptoin-server-production-6bc2.up.railway.app"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
 });
 
 const token = localStorage.getItem('jwt');
-
-api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+if (token) {
+  api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
 
 api.defaults.headers.post['Content-Type'] = 'application/json';
 
 export default api;
+
